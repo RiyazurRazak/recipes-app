d919b9ccdc0940244b7c139e76fa461f9f010f61
RiyazurRazak <riyazurrazaknoushath786@gmail.com>
2023-04-24 11:30:26 +0530

    Add project files.

---
+++ Models/RecipesModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace recipes_app.Models
+{
+    public class RecipesModel
+    {
+        [Key]
+        public string RecipeId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public DateTime CreatedDate { get; set; }
+
+        [ForeignKey("Author")]
+        public string AuthorId { get; set; }
+
+        public UsersModel Author { get; set; }
+
+    }
+}
